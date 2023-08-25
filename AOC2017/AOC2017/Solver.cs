4810ba64cf8a23608bf8d16a52a7a87826c4f491
Kirill Kostenkov <kiril.kostenkov@tripledotfortune.com>
2023-08-25 15:20:22 +0200

    Add next index calculator

--- AOC2017/AOC2017/Solver.cs
+++ AOC2017/AOC2017/Solver.cs
@@ -21,6 +21,31 @@ public class Solver
 
     public int SolveOppositeDigit(string captcha)
     {
-        throw new NotImplementedException();
+        var sum = 0;
+        var step = captcha.Length / 2;
+        for (var index = 0; index < captcha.Length; index++) {
+            var character = captcha[index];
+            char nextCharacter;
+            if (index + step == captcha.Length - 1) {
+                nextCharacter = captcha[0];
+            }
+            else {
+                nextCharacter = captcha[captcha.Length - index];
+            }
+            if (character == nextCharacter) {
+                sum += character - '0';
+            }
+        }
+        return sum;
+    }
+
+    public int GetIndexInRangeAfterSteps(int totalLength, int current, int addSteps)
+    {
+        var result = current + addSteps;
+        while (result > totalLength - 1) {
+            result -= totalLength;
+        }
+
+        return result;
     }
 }
--- AOC2017/Tests/SolverTest.cs
+++ AOC2017/Tests/SolverTest.cs
@@ -14,7 +14,7 @@ public class Tests
     
     [Test]
     [TestCase("1122", 6)]
-    // [TestCase("1111", 4)]
+    [TestCase("1221", 0)]
     // [TestCase("1234", 0)]
     // [TestCase("91212129", 9)]
     public void When_GivenCaptcha_Should_CalculateOppositeDigitSum(string captcha, int expectedResult)
@@ -22,5 +22,18 @@ public class Tests
         var solver = new Solver();
         var result = solver.SolveOppositeDigit(captcha);
         Assert.AreEqual(expectedResult, result);
+    }
+
+    [Test]
+    // [TestCase(0, 0, 0, 0)]
+    [TestCase(4, 0, 1, 1)]
+    [TestCase(4, 3, 1, 0)]
+    [TestCase(2, 0, 10, 0)]
+    
+    public void When_GivenRange_CalculatesNextIndex(int length, int current, int add, int expected)
+    {
+        var solver = new Solver();
+        var actual = solver.GetIndexInRangeAfterSteps(length, current, add);
+        Assert.AreEqual(expected, actual);
     }
 }
