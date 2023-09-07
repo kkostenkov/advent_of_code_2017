f844ff6603c0da004cb0574e9f31282dee39333d
Alexandra Kislaya <alexandra.kislaya@tripledotfortune.com>
2023-09-07 18:11:16 +0300

    wip

--- AOC2017/AOC2017/Solver.cs
+++ AOC2017/AOC2017/Solver.cs
@@ -1,13 +1,18 @@
 public class Solver
 {
-    public int Solve(string captcha)
+    public int SolveWithNext(string captcha)
+    {
+        return Solve(captcha, 1);
+    }
+    
+    private int Solve(string captcha, int indexOffset)
     {
         var sum = 0;
 
         for (var index = 0; index < captcha.Length; index++) {
             var character = captcha[index];
             var value = int.Parse(character.ToString());
-            var nextIndex = index + 1 == captcha.Length ? 0 : index + 1;
+            var nextIndex = index + indexOffset == captcha.Length ? 0 : index + indexOffset;
 
             var nextChar = captcha[nextIndex];
             var nextValue = int.Parse(nextChar.ToString());
@@ -16,5 +21,12 @@ public class Solver
             }
         }
         return sum;
+    }
+    
+    // private int GetIndex
+
+    public void SolveAndPrint(string captcha)
+    {
+        Console.WriteLine(SolveWithNext(captcha));
     }
 }
