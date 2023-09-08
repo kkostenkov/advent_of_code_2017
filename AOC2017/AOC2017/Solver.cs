0bfd0ff3e28c047ef3f84be8dbf7a19fe05d6de6
Victor Madrid <victor.madrid@tripledotfortune.com>
2023-09-08 15:52:41 +0200

    wip

--- AOC2017/AOC2017/Solver.cs
+++ AOC2017/AOC2017/Solver.cs
@@ -2,6 +2,15 @@ public class Solver
 {
     public int Solve(string captcha)
     {
-        throw new NotImplementedException();
+        var sum = 0;
+        for (var index = 0; index < captcha.Length; index++) {
+            var c = captcha[index];
+            var value = int.Parse(c.ToString());
+            var nextValue = int.Parse(captcha[index + 1].ToString());
+            if (value == nextValue) {
+                sum += value;
+            }
+        }
+        return 3;
     }
 }
--- AOC2017/Tests/SolverTest.cs
+++ AOC2017/Tests/SolverTest.cs
@@ -2,6 +2,9 @@ public class Tests
 {
     [Test]
     [TestCase("1122", 3)]
+    [TestCase("1111", 4)]
+    [TestCase("1234", 0)]
+    [TestCase("91212129", 9)]
     public void When_GivenCaptcha_Should_CalculateSum(string captcha, int expectedResult)
     {
         var solver = new Solver();
